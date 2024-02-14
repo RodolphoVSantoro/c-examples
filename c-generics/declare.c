47e117e669ec2912136f0a752b9eb2c9ca6e3788
RodolphoVSantoro <rodolphovs@gmail.com>
2024-02-14 13:50:30 -0300

    add code

--- c-generics/declare.c
+++ c-generics/declare.c
@@ -11,14 +11,23 @@ typedef enum { OK = 0,
 
 DECLARE_RESULT(int, int_result);
 
+DECLARE_RESULT(float, float_result);
+
 int_result test() {
     int_result ret = {1, OK};
+    return ret;
+}
+
+float_result test2() {
+    float_result ret = {1.0, ERROR};
     return ret;
 }
 
 int main() {
     int_result d = test();
     printf("%d %d\n", d.result, d.error);
+    float_result f = test2();
+    printf("%f %d\n", f.result, f.error);
 
     return 0;
 }
